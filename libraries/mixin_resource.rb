9455183f2d0743407bba86ad367b6d2481f96462
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-03-17 11:50:23 -0300

    remove errors in crontab

--- libraries/mixin_resource.rb
+++ libraries/mixin_resource.rb
@@ -54,7 +54,7 @@ module MoBackup
       end
 
       def backup_command(model)
-        "/bin/bash -lc \"backup perform -q --trigger #{model}\""
+        "/bin/bash -lc \"backup perform -q --trigger #{model}\" >/dev/null 2>&1"
       end
 
     end
